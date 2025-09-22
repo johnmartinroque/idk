1fadf9f87671bedefd512e3da8f3d88d8b080edd
johnmartinroque <johnmartinroque@gmail.com>
2025-09-22 14:26:50 +0800

    register component

--- client/src/screens/Authentication.jsx
+++ client/src/screens/Authentication.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "../components/authentication/Login";
+import Register from "../components/authentication/Register";
 
 function Authentication() {
   return (
     <div className="flex flex-col items-center">
       <h1>Authentication</h1>
-      <Login className="w-full" />
+      <Login />
+      <Register />
     </div>
   );
 }
